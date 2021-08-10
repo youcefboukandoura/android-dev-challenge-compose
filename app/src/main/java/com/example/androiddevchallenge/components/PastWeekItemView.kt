a75abbba241924d786a04d2f229f9807f46e57c1
youcefboukandoura <youcef.boukandoura@toptal.com>
2021-08-10 13:33:22 +0100

    Day 3 : update ui of past week

--- app/src/main/java/com/example/androiddevchallenge/components/PastWeekItemView.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/PastWeekItemView.kt
@@ -1,15 +1,16 @@
 package com.example.androiddevchallenge.components
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,23 +22,33 @@ import com.example.androiddevchallenge.ui.theme.MyTheme
  */
 @Composable
 fun PastWeekItemView(item: Week) {
+
+    val stroke = Stroke(
+        width = 2f,
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    )
+    println(item)
     Box(
         modifier = Modifier
-            .background(DarkGray)
             .width(90.dp)
-            .height(64.dp)
-            .padding(0.dp, 4.dp)
-            .border(1.dp, Color.Gray, shape = RoundedCornerShape(4.dp)),
+            .height(90.dp)
+            .padding(8.dp),
 
         contentAlignment = Alignment.Center
     ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            drawRoundRect(color = Color.Gray, style = stroke, cornerRadius = CornerRadius(8f, 8f))
+        }
         Column {
             Spacer(modifier = Modifier.weight(1f))
             Text(
 
-                text = item.weekDay,
+                text = item.weekDay.toString(),
                 Modifier.fillMaxWidth(),
-
                 textAlign = TextAlign.Center,
             )
             Text(
@@ -50,6 +61,7 @@ fun PastWeekItemView(item: Week) {
                 Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
             )
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
--- app/src/main/java/com/example/androiddevchallenge/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/viewmodel/MainViewModel.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.example.androiddevchallenge.model.Week
+import com.example.androiddevchallenge.sample.generateWeekList
 
 class MainViewModel {
 
-    val weekList: List<Week> by mutableStateOf(listOf(Week("SAT", "1", "JUL")))
+    val weekList: List<Week> by mutableStateOf(generateWeekList())
 
     var selectedWeek: Week? by mutableStateOf(null)
 
