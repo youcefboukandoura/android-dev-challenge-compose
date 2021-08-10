4c8c2aaf82655cca2367b2ac332340992b4985de
Michel Souza <84324145+michelhf@users.noreply.github.com>
2021-08-10 14:39:21 +0200

    Replace isSelected by type

--- app/src/main/java/com/example/androiddevchallenge/model/Week.kt
+++ app/src/main/java/com/example/androiddevchallenge/model/Week.kt
@@ -4,5 +4,10 @@ data class Week(
     val weekDay: String,
     val day: String,
     val month: String,
-    val isSelected: Boolean = false
+    val type: WeekType
 )
+
+sealed class WeekType() {
+    data class PastWeek(val isSelected: Boolean): WeekType()
+    data class FutureWeek(val isSelected: Boolean): WeekType()
+}
